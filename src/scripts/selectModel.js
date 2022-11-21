7888e6450bafaa9ce5a5239d2b67da418057d547
Dastan <dastan132000@mail.ru>
2022-11-21 09:54:05 +0600

    fix: поправил устаревший HTML

--- src/scripts/selectModel.js
+++ src/scripts/selectModel.js
@@ -1,57 +1,58 @@
 import {
-  mainProductImage,
-  productColorTabImages, productColorTabs,
-  productModelTabs,
-  productModelTabsContainer, productThumbImages, productThumbs,
-} from "./constants";
-import {store} from "./store";
-import {data} from "./data";
-import {toggleThumbs} from "./handlePreviews";
-import {changeProductData} from "./changeProductData";
+    mainProductImage,
+    productColorTabImages, productColorTabs,
+    productModelTabs,
+    productModelTabsContainer, productThumbImages, productThumbs,
+} from './constants';
+import { store } from './store';
+import { data } from './data';
+import { toggleThumbs } from './handlePreviews';
+import { changeProductData } from './changeProductData';
 
 function selectModel() {
-  productModelTabsContainer.addEventListener('click', ({target}) => {
-    if (target.tagName === 'BUTTON') {
+    productModelTabsContainer.addEventListener('click', ({ target }) => {
+        if (target.tagName === 'BUTTON') {
 
-      productModelTabs.forEach((item) => {
-        item.classList.remove('product__model-tab_active');
-      });
-      target.classList.add('product__model-tab_active');
+            productModelTabs.forEach((item) => {
+                item.classList.remove('product__model-tab_active');
+            });
+            target.classList.add('product__model-tab_active');
 
-      toggleThumbs(productThumbs[0])
+            toggleThumbs(productThumbs[0])
 
-      productColorTabs.forEach((item) => {
-        item.classList.remove('product__color-tab_active');
-      });
+            productColorTabs.forEach((item) => {
+                item.classList.remove('product__color-tab_active');
+            });
 
-      productColorTabs[0].classList.add('product__color-tab_active')
+            productColorTabs[0].classList.add('product__color-tab_active')
+            console.log(target.dataset.model)
 
+            store.model = target.dataset.model
 
-      store.model = target.dataset.model
+            const suitableModels = data.filter(obj => obj.model === store.model);
+            console.log(suitableModels)
+            const firstModel = suitableModels[0];
+            const { model, description, name, price, id, memory } = firstModel;
+            changeProductData(model, name, price, id, description, memory);
 
-      const suitableModels = data.filter(obj => obj.model === store.model);
-      const firstModel = suitableModels[0];
-      const {model, description, name, price, id, memory} = firstModel;
-      changeProductData(model, name, price, id, description, memory);
+            firstModel.photos.forEach((preview, index) => {
+                productThumbImages[index].src = preview.thumbnail
+                productThumbImages[index].dataset.src = preview.thumbnail
+            });
 
-      firstModel.photos.forEach((preview, index) => {
-        productThumbImages[index].src = preview.thumbnail
-        productThumbImages[index].dataset.src = preview.thumbnail
-      });
+            suitableModels.forEach((model, index) => {
+                productColorTabs[index].dataset.color = model.color;
+            })
 
-      suitableModels.forEach((model, index) => {
-        productColorTabs[index].dataset.color = model.color;
-      })
+            const colorPreviews = suitableModels.map(model => model.mainPhoto)
 
-      const colorPreviews = suitableModels.map(model => model.mainPhoto)
+            colorPreviews.forEach((image, i) => {
+                productColorTabImages[i].src = image
+                mainProductImage.src = colorPreviews[0]
+            });
 
-      colorPreviews.forEach((image, i) => {
-        productColorTabImages[i].src = image
-        mainProductImage.src = colorPreviews[0]
-      });
-
-    }
-  })
+        }
+    })
 }
 
 selectModel()
